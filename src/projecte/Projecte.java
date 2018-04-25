dd6443bc2f01179ed5ede19f6bbe474faddc96ac
alumne <alumne@alumne-PC>
2018-04-25 12:28:02 +0200

    he fet algo

--- src/projecte/Projecte.java
+++ src/projecte/Projecte.java
@@ -4,6 +4,8 @@
  * and open the template in the editor.
  */
 package projecte;
+import java.util.Scanner;
+import java.util.Random;
 
 /**
  *
@@ -11,11 +13,51 @@ package projecte;
  */
 public class Projecte {
 
-    /**
+    /** Projecte 
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       
+      
+    int num= 0,repetits,correcte;    
+        
+    boolean valor = true;     
+   
+    int[][] cartro = new int[3][6];
+    int[] sorteig = new int[99];   
+
+    
+        System.out.println(sorteig);
+        
+        
+            System.out.println(sorteig);
+//            repetits = repetits(sorteig);//falta crear un metode de repetits
+//            correcte(repetits, cartro); //falta crear un metode de si es correcte
+//            boolen ordena=true;
+        
+           // Random rnd = new Random();
+            
+           
+           
+           
+           
+           
+      
     }
     
-}
+     
+        for (int i = 0; i < 100; i++) {
+          System.out.println(sorteig.nextDouble());;
+    }
+            
+     }       
+            
+            
+            
+    
+
+  
+
+
+    
+
